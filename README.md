7e3e149a1f57bf5a22743be0e805148a6af00c08
Mukesh Kumar <mkmehta2041@gmail.com>
2023-10-15 20:16:19 +0530

    styling emails

--- README.md
+++ README.md
@@ -1204,3 +1204,44 @@ add this on .gitignore file
 # react-email
 .react-email/
 ```
+
+### Styling Emails
+
+```jsx
+import React from "react";
+import {
+  Html,
+  Body,
+  Container,
+  Tailwind,
+  Text,
+  Link,
+  Preview,
+} from "@react-email/components";
+
+const WelcomeTempalte = ({ name }: { name: string }) => {
+  return (
+    <Html>
+      <Preview>Welcome abroad! </Preview>
+      <Tailwind>
+        <Body className="bg-white">
+          <Container>
+            <Text className="font-bold text-3xl"> Hello {name}</Text>
+            <Link href="https://www.google.com">www.google.com</Link>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+};
+
+const body = {
+  background: "#fff",
+};
+
+const heading = {
+  fontSize: "32px",
+};
+
+export default WelcomeTempalte;
+```
--- emails/WelcomeTempalte.tsx
+++ emails/WelcomeTempalte.tsx
@@ -3,6 +3,7 @@ import {
   Html,
   Body,
   Container,
+  Tailwind,
   Text,
   Link,
   Preview,
@@ -12,14 +13,24 @@ const WelcomeTempalte = ({ name }: { name: string }) => {
   return (
     <Html>
       <Preview>Welcome abroad! </Preview>
-      <Body>
-        <Container>
-          <Text> Hello {name}</Text>
-          <Link href="https://www.google.com">www.google.com</Link>
-        </Container>
-      </Body>
+      <Tailwind>
+        <Body className="bg-white">
+          <Container>
+            <Text className="font-bold text-3xl"> Hello {name}</Text>
+            <Link href="https://www.google.com">www.google.com</Link>
+          </Container>
+        </Body>
+      </Tailwind>
     </Html>
   );
+};
+
+const body = {
+  background: "#fff",
+};
+
+const heading = {
+  fontSize: "32px",
 };
 
 export default WelcomeTempalte;
